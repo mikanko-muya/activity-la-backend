c9cff82f45941574db2cca05ba9d1cdafbe6a9da
mikanko-muya <mikanko60@gmail.com>
2026-09-09 11:46:59 +0700

    modified:   src/controllers/venue.controller.js

--- src/controllers/venue.controller.js
+++ src/controllers/venue.controller.js
@@ -1,12 +1,15 @@
-import { SendError } from "../utils/response.js";
-
+import { SendError, SendSuccess } from "../utils/response.js";
+import { prisma} from "../config/prisma.js"
 export default class venueController{
-    static async getVenues(req, res){
+    static async getAllVenues(req, res){
         try {
-            
+            const venues = await prisma.venue.findMany()
+            return SendSuccess(res, 'Venues retrieved successfully', venues)
             
         } catch (error) {
             return SendError(res, 500, 'Server internal', error)
         }
     }
+
+    static async create
 }
